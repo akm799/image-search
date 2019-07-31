1275786422765c0c0da08e2a7d0b2c021fd19f3f
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-07-31 21:08:06 +0100

    Re-factored internal window tracking outside the parent view class.

--- app/src/main/java/uk/co/akm/test/imagesearch/view/DynamicWindowView.java
+++ app/src/main/java/uk/co/akm/test/imagesearch/view/DynamicWindowView.java
@@ -22,25 +22,11 @@ public class DynamicWindowView extends View {
     private static final int DEFAULT_WINDOW_BORDER_COLOUR = Color.RED;
     private static final float DEFAULT_WINDOW_INITIAL_SIDE_FRACTION = 0.1f;
 
-    private static final float NO_VALUE = -1f;
-
-
-    private float wLeft = NO_VALUE;
-    private float wTop = NO_VALUE;
-    private float wWidth = NO_VALUE;
-    private float wHeight = NO_VALUE;
-
-    private float xPos = NO_VALUE;
-    private float yPos = NO_VALUE;
-
-    private float xMin = NO_VALUE;
-    private float xMax = NO_VALUE;
-    private float yMin = NO_VALUE;
-    private float yMax = NO_VALUE;
-
     private Paint windowPaint;
     private Paint borderPaint;
     private float windowInitialSideFraction;
+
+    private InternalWindow window;
 
     public DynamicWindowView(Context context) {
         super(context);
@@ -103,8 +89,8 @@ public class DynamicWindowView extends View {
             drawBorder(canvas);
         }
 
-        if (wLeft != NO_VALUE) {
-            drawWindow(canvas);
+        if (window != null) {
+            window.draw(canvas, windowPaint);
         }
     }
 
@@ -112,24 +98,20 @@ public class DynamicWindowView extends View {
         canvas.drawRect(0, 0, getWidth(), getHeight(), borderPaint);
     }
 
-    private void drawWindow(Canvas canvas) {
-        canvas.drawRect(wLeft, wTop, wLeft + wWidth, wTop + wHeight, windowPaint);
-    }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (isEnabled()) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_UP:
-                    clearTracker();
+                    stopTracking();
                     break;
 
                 case MotionEvent.ACTION_DOWN:
-                    processTouch(event);
+                    createWindow(event);
                     break;
 
                 case MotionEvent.ACTION_MOVE:
-                    processMove(event);
+                    onChangePositionOrSizeTouchEvent(event);
             }
 
             return true;
@@ -138,149 +120,21 @@ public class DynamicWindowView extends View {
         }
     }
 
-    private void clearTracker() {
-        clearSizeTracker();
-        clearPositionTracker();
-    }
-
-    private void clearPositionTracker() {
-        xPos = NO_VALUE;
-        yPos = NO_VALUE;
-    }
-
-    private void clearSizeTracker() {
-        xMin = NO_VALUE;
-        xMax = NO_VALUE;
-        yMin = NO_VALUE;
-        yMax = NO_VALUE;
-    }
-
-    private void processTouch(MotionEvent event) {
-        if (wLeft == NO_VALUE) {
-            initWindow(event.getX(), event.getY());
+    private void stopTracking() {
+        if (window != null) {
+            window.stopTracking();
         }
     }
 
-    private void initWindow(float x, float y) {
-        final float side = windowInitialSideFraction*Math.min(getWidth(), getHeight());
-        final float halfSide = side/2;
-        final float initLeft = x - halfSide;
-        final float initTop = y - halfSide;
-
-        if (inRange(initLeft, side, getWidth()) && inRange(initTop, side, getHeight())) {
-            wWidth = side;
-            wHeight = side;
-            wLeft = initLeft;
-            wTop = initTop;
-            invalidate();
+    private void createWindow(MotionEvent event) {
+        if (window == null && InternalWindow.canInitializeWindow(this, event, windowInitialSideFraction)) {
+            window = new InternalWindow(this, event, windowInitialSideFraction);
         }
     }
 
-    private void processMove(MotionEvent event) {
-        switch (event.getPointerCount()) {
-            case 1:
-                processPositionChangeMove(event);
-                break;
-
-            case 2:
-                processSizeChangeMove(event);
-                break;
+    private void onChangePositionOrSizeTouchEvent(MotionEvent event) {
+        if (window != null) {
+            window.onChangePositionOrSizeTouchEvent(event);
         }
-    }
-
-    private void processPositionChangeMove(MotionEvent event) {
-        final float xPosNew = event.getX();
-        final float yPosNew = event.getY();
-
-        if (xPos != NO_VALUE) {
-            changePosition(xPosNew, yPosNew);
-        }
-
-        xPos = xPosNew;
-        yPos = yPosNew;
-    }
-
-    private void changePosition(float xPosNew, float yPosNew) {
-        final float dx = xPosNew - xPos;
-        final float dy = yPosNew - yPos;
-
-        final float newLeft = wLeft + dx;
-        final float newTop = wTop + dy;
-
-        if (inRange(newLeft, wWidth, getWidth()) && inRange(newTop, wHeight, getHeight())) {
-            wLeft = newLeft;
-            wTop = newTop;
-            invalidate();
-        }
-    }
-
-    private void processSizeChangeMove(MotionEvent event) {
-        final float x1 = event.getX(0);
-        final float x2 = event.getX(1);
-        final float dx = Math.abs(x2 - x1);
-
-        final float y1 = event.getY(0);
-        final float y2 = event.getY(1);
-        final float dy = Math.abs(y2 - y1);
-
-        if (dx > dy) {
-            processWidthChangeMove(x1, x2);
-        } else {
-            processHeightChangeMove(y1, y2);
-        }
-    }
-
-    private void processWidthChangeMove(float x1, float x2) {
-        final float xMinNew = Math.min(x1, x2);
-        final float xMaxNew = Math.max(x1, x2);
-
-        if (xMin != NO_VALUE) {
-            changeWidth(xMinNew, xMaxNew);
-        }
-
-        xMin = xMinNew;
-        xMax = xMaxNew;
-    }
-
-    private void changeWidth(float xMinNew, float xMaxNew) {
-        final float dx = (xMaxNew - xMinNew) - (xMax - xMin);
-        final float newWidth = wWidth + dx;
-        if (newWidth > 0) {
-            final float newLeft = wLeft - dx/2;
-            if (inRange(newLeft, newWidth, getWidth())) {
-                wLeft = newLeft;
-                wWidth = newWidth;
-                invalidate();
-            }
-        }
-    }
-
-    private void processHeightChangeMove(float y1, float y2) {
-        final float yMinNew = Math.min(y1, y2);
-        final float yMaxNew = Math.max(y1, y2);
-
-        if (yMin != NO_VALUE) {
-            changeHeight(yMinNew, yMaxNew);
-        }
-
-        yMin = yMinNew;
-        yMax = yMaxNew;
-    }
-
-    private void changeHeight(float yMinNew, float yMaxNew) {
-        final float dy = (yMaxNew - yMinNew) - (yMax - yMin);
-        final float newHeight = wHeight + dy;
-        if (newHeight > 0) {
-            final float newTop = wTop - dy/2;
-            if (inRange(newTop, newHeight, getHeight())) {
-                wTop = newTop;
-                wHeight = newHeight;
-                invalidate();
-            }
-        }
-    }
-
-    private boolean inRange(float start, float size, int maxSize) {
-        return start > 0 && start + size < maxSize;
     }
 }
---
+++ app/src/main/java/uk/co/akm/test/imagesearch/view/InternalWindow.java
@@ -0,0 +1,186 @@
+package uk.co.akm.test.imagesearch.view;
+
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Represents the state of a rectangular window within a parent view and has methods that move and
+ * change the size of this window (always within the parent view) in response to touch events in the
+ * parent view.
+ */
+final class InternalWindow {
+    private static final float NO_VALUE = -1f;
+
+    private float wLeft = NO_VALUE;
+    private float wTop = NO_VALUE;
+    private float wWidth = NO_VALUE;
+    private float wHeight = NO_VALUE;
+
+    private float xPos = NO_VALUE;
+    private float yPos = NO_VALUE;
+
+    private float xMin = NO_VALUE;
+    private float xMax = NO_VALUE;
+    private float yMin = NO_VALUE;
+    private float yMax = NO_VALUE;
+
+    private final View parent;
+
+    static boolean canInitializeWindow(View parent, MotionEvent event, float windowInitialSideFraction) {
+        return canInitializeWindow(parent, event.getX(), event.getY(), windowInitialSideFraction);
+    }
+
+    private static boolean canInitializeWindow(View parent, float x, float y, float windowInitialSideFraction) {
+        final float side = windowInitialSideFraction*Math.min(parent.getWidth(), parent.getHeight());
+        final float halfSide = side/2;
+        final float initLeft = x - halfSide;
+        final float initTop = y - halfSide;
+
+        return (inRange(initLeft, side, parent.getWidth()) && inRange(initTop, side, parent.getHeight()));
+    }
+
+    InternalWindow(View parent, MotionEvent event, float windowInitialSideFraction) {
+        this.parent = parent;
+        initWindow(event.getX(), event.getY(), windowInitialSideFraction);
+    }
+
+    private void initWindow(float x, float y, float windowInitialSideFraction) {
+        final float side = windowInitialSideFraction*Math.min(parent.getWidth(), parent.getHeight());
+        final float halfSide = side/2;
+        final float initLeft = x - halfSide;
+        final float initTop = y - halfSide;
+
+        if (inRange(initLeft, side, parent.getWidth()) && inRange(initTop, side, parent.getHeight())) {
+            wWidth = side;
+            wHeight = side;
+            wLeft = initLeft;
+            wTop = initTop;
+            parent.invalidate();
+        }
+    }
+
+    void draw(Canvas canvas, Paint paint) {
+        canvas.drawRect(wLeft, wTop, wLeft + wWidth, wTop + wHeight, paint);
+    }
+
+    void stopTracking() {
+        xPos = NO_VALUE;
+        yPos = NO_VALUE;
+
+        xMin = NO_VALUE;
+        xMax = NO_VALUE;
+        yMin = NO_VALUE;
+        yMax = NO_VALUE;
+    }
+
+    void onChangePositionOrSizeTouchEvent(MotionEvent event) {
+        switch (event.getPointerCount()) {
+            case 1:
+                onPositionChangeTouchEvent(event);
+                break;
+
+            case 2:
+                onSizeChangeTouchEvent(event);
+                break;
+        }
+    }
+
+    private void onPositionChangeTouchEvent(MotionEvent event) {
+        final float xPosNew = event.getX();
+        final float yPosNew = event.getY();
+
+        if (xPos != NO_VALUE) {
+            changePosition(xPosNew, yPosNew);
+        }
+
+        xPos = xPosNew;
+        yPos = yPosNew;
+    }
+
+    private void changePosition(float xPosNew, float yPosNew) {
+        final float dx = xPosNew - xPos;
+        final float dy = yPosNew - yPos;
+
+        final float newLeft = wLeft + dx;
+        final float newTop = wTop + dy;
+
+        if (inRange(newLeft, wWidth, parent.getWidth()) && inRange(newTop, wHeight, parent.getHeight())) {
+            wLeft = newLeft;
+            wTop = newTop;
+            parent.invalidate();
+        }
+    }
+
+    private void onSizeChangeTouchEvent(MotionEvent event) {
+        final float x1 = event.getX(0);
+        final float x2 = event.getX(1);
+        final float dx = Math.abs(x2 - x1);
+
+        final float y1 = event.getY(0);
+        final float y2 = event.getY(1);
+        final float dy = Math.abs(y2 - y1);
+
+        if (dx > dy) {
+            processWidthChangeMove(x1, x2);
+        } else {
+            processHeightChangeMove(y1, y2);
+        }
+    }
+
+    private void processWidthChangeMove(float x1, float x2) {
+        final float xMinNew = Math.min(x1, x2);
+        final float xMaxNew = Math.max(x1, x2);
+
+        if (xMin != NO_VALUE) {
+            changeWidth(xMinNew, xMaxNew);
+        }
+
+        xMin = xMinNew;
+        xMax = xMaxNew;
+    }
+
+    private void changeWidth(float xMinNew, float xMaxNew) {
+        final float dx = (xMaxNew - xMinNew) - (xMax - xMin);
+        final float newWidth = wWidth + dx;
+        if (newWidth > 0) {
+            final float newLeft = wLeft - dx/2;
+            if (inRange(newLeft, newWidth, parent.getWidth())) {
+                wLeft = newLeft;
+                wWidth = newWidth;
+                parent.invalidate();
+            }
+        }
+    }
+
+    private void processHeightChangeMove(float y1, float y2) {
+        final float yMinNew = Math.min(y1, y2);
+        final float yMaxNew = Math.max(y1, y2);
+
+        if (yMin != NO_VALUE) {
+            changeHeight(yMinNew, yMaxNew);
+        }
+
+        yMin = yMinNew;
+        yMax = yMaxNew;
+    }
+
+    private void changeHeight(float yMinNew, float yMaxNew) {
+        final float dy = (yMaxNew - yMinNew) - (yMax - yMin);
+        final float newHeight = wHeight + dy;
+        if (newHeight > 0) {
+            final float newTop = wTop - dy/2;
+            if (inRange(newTop, newHeight, parent.getHeight())) {
+                wTop = newTop;
+                wHeight = newHeight;
+                parent.invalidate();
+            }
+        }
+    }
+
+    private static boolean inRange(float start, float size, int maxSize) {
+        return start > 0 && start + size < maxSize;
+    }
+}
