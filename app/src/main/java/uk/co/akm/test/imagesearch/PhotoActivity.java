93269516f57c6719635aafc747f940c3b045fa3b
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-08-14 21:27:58 +0100

    Added template for photo display view.

--- app/src/main/java/uk/co/akm/test/imagesearch/PhotoActivity.java
+++ app/src/main/java/uk/co/akm/test/imagesearch/PhotoActivity.java
@@ -7,16 +7,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import uk.co.akm.test.imagesearch.photo.PhotoFunctions;
 import uk.co.akm.test.imagesearch.photo.impl.FilePhotoFunctions;
+import uk.co.akm.test.imagesearch.view.display.PhotoWindowView;
 
 public class PhotoActivity extends AppCompatActivity {
     private final PhotoFunctions photoFunctions = new FilePhotoFunctions();
 
-    private ImageView imageView;
+    private PhotoWindowView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +35,7 @@ public class PhotoActivity extends AppCompatActivity {
         if (photoFunctions.imageCaptured(requestCode, resultCode)) {
             try {
                 final Bitmap bitmap = photoFunctions.readCapturedImage(this);
-                imageView.setImageBitmap(rotateBitmap(bitmap));
+                imageView.setPhoto(rotateBitmap(bitmap));
             } catch (Exception e) {
                 Log.e("PhotoActivity", "Error while reading the captured image.", e);
                 Toast.makeText(this, "Could not read the captured image.", Toast.LENGTH_SHORT).show();
--- app/src/main/java/uk/co/akm/test/imagesearch/view/display/PhotoWindowView.java
+++ app/src/main/java/uk/co/akm/test/imagesearch/view/display/PhotoWindowView.java
@@ -1,11 +1,15 @@
 package uk.co.akm.test.imagesearch.view.display;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class PhotoWindowView extends View {
+    private Bitmap photo;
 
     public PhotoWindowView(Context context) {
         super(context);
@@ -23,4 +27,32 @@ public class PhotoWindowView extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (photo != null) {
+            final Rect dest = buildDestinationRectangle(photo);
+            canvas.drawBitmap(photo, null, dest, null);
+        }
+    }
+
+    private Rect buildDestinationRectangle(Bitmap bitmap) {
+        final float bitmapAspectRatio = (float)bitmap.getWidth()/bitmap.getHeight();
+
+        float height = Math.round(getWidth()*bitmapAspectRatio);
+        if (height <= getHeight()) {
+            final int topMargin = Math.round((getHeight() - height) / 2);
+
+            return new Rect(0, topMargin, getWidth(), getHeight() - topMargin);
+        } else {
+            float width = Math.round(getHeight()/bitmapAspectRatio);
+            final int leftMargin = Math.round((getWidth() - width) / 2);
+
+            return new Rect(leftMargin, 0, getWidth() - leftMargin, getHeight());
+        }
+    }
 }
