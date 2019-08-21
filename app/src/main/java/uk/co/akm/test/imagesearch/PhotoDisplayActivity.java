940f2ef3cf17ab3902ab0b24ddf9a899098ad233
Thanos Mavroidis <thanos.mavroidis@travelex.com>
2019-08-21 11:37:50 +0100

    Used the persisent PhotoView.

--- app/src/main/java/uk/co/akm/test/imagesearch/PhotoDisplayActivity.java
+++ app/src/main/java/uk/co/akm/test/imagesearch/PhotoDisplayActivity.java
@@ -2,18 +2,14 @@ package uk.co.akm.test.imagesearch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
-import uk.co.akm.test.imagesearch.photo.PhotoReader;
-import uk.co.akm.test.imagesearch.photo.impl.PhotoHandlerImpl;
-import uk.co.akm.test.imagesearch.view.display.PhotoWindowView;
+import uk.co.akm.test.imagesearch.view.display.persistent.PhotoWindowView;
 
 public class PhotoDisplayActivity extends AppCompatActivity {
-    private final PhotoReader photoReader = new PhotoHandlerImpl();
 
     static void start(Activity parent) {
         parent.startActivity(new Intent(parent, PhotoDisplayActivity.class));
@@ -29,11 +25,10 @@ public class PhotoDisplayActivity extends AppCompatActivity {
 
     private void displayCapturedPhoto(PhotoWindowView imageView) {
         try {
-            final Bitmap bitmap = photoReader.readCapturedImage(this, PhotoCaptureActivity.PHOTO_NAME);
-            imageView.setPhoto(bitmap);
+            imageView.setPhoto(PhotoCaptureActivity.PHOTO_NAME);
         } catch (Exception e) {
-            Log.e("PhotoDisplayActivity", "Error while reading the captured image.", e);
-            Toast.makeText(this, "Could not read the captured image.", Toast.LENGTH_SHORT).show();
+            Log.e("PhotoDisplayActivity", "Error while displaying the captured image.", e);
+            Toast.makeText(this, "Could not display the captured image.", Toast.LENGTH_SHORT).show();
         }
     }
 }
