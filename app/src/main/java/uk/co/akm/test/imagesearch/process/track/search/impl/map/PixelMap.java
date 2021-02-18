872c72d07c8d6d38deb9f590843b49f088815aa2
thanos-lush <thanos@lush.co.uk>
2021-02-18 18:51:25 +0000

    Improved brute force best match finder.

--- app/src/main/java/uk/co/akm/test/imagesearch/process/track/search/impl/map/PixelMap.java
+++ app/src/main/java/uk/co/akm/test/imagesearch/process/track/search/impl/map/PixelMap.java
@@ -2,11 +2,14 @@ package uk.co.akm.test.imagesearch.process.track.search.impl.map;
 
 public final class PixelMap {
     private final int width;
+    private final int height;
     private final int[] values;
 
     public PixelMap(int width, int[] values) {
         checkArguments(width, values);
+
         this.width = width;
+        this.height = values.length/width;
         this.values = values;
     }
 
@@ -20,6 +23,14 @@ public final class PixelMap {
         if ((float)i != f) {
             throw new IllegalArgumentException("Input 'values' array length (" + values.length + ") must be a multiple of the input 'width' (" + width + ").");
         }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public int getPixel(int x, int y) {
